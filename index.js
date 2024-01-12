827ec9eaf7ff4fbba7630a3bb2da7dbe90b85769
rahul kumar <rahulkumarkudra2004@gmail.com>
2024-01-12 15:09:50 +0530

    improve its css and also work on js file
    today i improved in css like set min and max height and also imrove color or background color and work on js try to add one more button which is edit but cann't i will try next time to add edit and and write their js.
    that's all.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", (event) => {
+    event.preventDefault();
     let list = document.querySelector(".list");
     let input = document.querySelector("input").value;
-    let div = document.createElement("div");
+
+     if(input=="") alert("enter task");
+    else{
+        let div = document.createElement("div");
     let newchild = list.appendChild(div);
     newchild.className = "iteam";
     newchild.innerText = input;
-
-
+    let key=0;
+    localStorage.setItem("key",newchild.innerText);
+    key++;
     let icon = document.createElement("i");
     icon.className = "fa-solid fa-circle-xmark";
     let button = document.createElement("button");
@@ -21,6 +26,11 @@ btn.addEventListener("click", (event) => {
 
     button.addEventListener("click", (event) => {
         document.querySelector(".iteam").parentNode.removeChild(document.querySelector(".iteam"))
-    })
 
+        let myTodoList = document.querySelectorAll('.item');
+
+
+    })
+    
+}
 })
